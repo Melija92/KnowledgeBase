25ef9128e6dbff1197c90aec8e1355a18fc37a4e
Dheeraj Kumar Gunti <DivyaDheeraj@DESKTOP-L5RBQ6F>
2016-07-19 20:30:08 +0530

    Modified few things.

--- KnowledgeBase/Scripts/articlesearch.js
+++ KnowledgeBase/Scripts/articlesearch.js
@@ -1,6 +1,8 @@
 /* Open when someone clicks on the span element */
 function openArticles() {
     document.getElementById("articleNav").style.width = "100%";
+    $('#search').focus();
+    $('#table').hide();
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
@@ -9,16 +11,19 @@ function closeArticles() {
 }
 
 $(function () {
-    var $rows = $('#table tr');
     $('#search').keyup(function () {
-
-        var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
-            reg = RegExp(val, 'i'),
-            text;
-
-        $rows.show().filter(function () {
-            text = $(this).text().replace(/\s+/g, ' ');
-            return !reg.test(text);
-        }).hide();
+        if ($('#search').val() == '')
+        { $('#table').hide(); }
+        else {
+            $('#table').show();
+            var $rows = $('#table tr');
+            var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+                reg = RegExp(val, 'i'),
+                text;
+            $rows.show().filter(function () {
+                text = $(this).text().replace(/\s+/g, ' ');
+                return !reg.test(text);
+            }).hide();
+        }
     });
 });
