ca56d607e496517e7b537bfbf21230d226732dd7
Dheeraj Kumar Gunti <DivyaDheeraj@DESKTOP-L5RBQ6F>
2016-08-01 19:36:36 +0530

    Modified Default Page.

--- KnowledgeBase/App_Start/RouteConfig.cs
+++ KnowledgeBase/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace KnowledgeBase
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "MVC", action = "WorkingwithEntityFrameworkinNetCore", id = UrlParameter.Optional }
+                defaults: new { controller = "MVC", action = "WorkingwithAzureStorageinWebApplication", id = UrlParameter.Optional }
             );
         }
     }
